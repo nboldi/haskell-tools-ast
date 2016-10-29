a354cd4c31239266b4f6fb1a5f57f56e8815267a
nboldi <nboldi@elte.hu>
2016-10-29 19:40:54 +0200

    Refactoring in FromGHC, and simplify Refactor API exporting

--- src/ast-ghc/Language/Haskell/Tools/AST/FromGHC.hs
+++ src/ast-ghc/Language/Haskell/Tools/AST/FromGHC.hs
@@ -2,13 +2,8 @@
 -- the Ghc monad. The conversion can be performed from the Parsed and the Renamed GHC AST. If the renamed AST 
 -- is given, additional semantic information is looked up while traversing the AST. 
 module Language.Haskell.Tools.AST.FromGHC 
-  ( module Language.Haskell.Tools.AST.FromGHC.Modules
-  , module Language.Haskell.Tools.AST.FromGHC.Monad
-  , module Language.Haskell.Tools.AST.FromGHC.Names
-  , module Language.Haskell.Tools.AST.FromGHC.Utils
-  ) where
+  ( trfModule, trfModuleRename, addTypeInfos, runTrf ) where
 
 import Language.Haskell.Tools.AST.FromGHC.Modules
 import Language.Haskell.Tools.AST.FromGHC.Monad
-import Language.Haskell.Tools.AST.FromGHC.Names
-import Language.Haskell.Tools.AST.FromGHC.Utils
+import Language.Haskell.Tools.AST.FromGHC.AddTypeInfo
