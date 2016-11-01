dd9c578387e618236a44301e6c801da27e4c3349
nboldi <nboldi@elte.hu>
2016-11-01 15:40:53 +0100

    improvement in ast-gen API

--- src/ast-gen/Language/Haskell/Tools/AST/Rewrite.hs
+++ src/ast-gen/Language/Haskell/Tools/AST/Rewrite.hs
@@ -2,53 +2,11 @@
 -- Contains modules for constructing parts of the AST and modules 
 -- for pattern matching (deconstructing) parts of the AST.
 module Language.Haskell.Tools.AST.Rewrite 
-  ( module Language.Haskell.Tools.AST.Gen.Modules
-  , module Language.Haskell.Tools.AST.Gen.Decls
-  , module Language.Haskell.Tools.AST.Gen.Binds
-  , module Language.Haskell.Tools.AST.Gen.Types
-  , module Language.Haskell.Tools.AST.Gen.Kinds
-  , module Language.Haskell.Tools.AST.Gen.Exprs
-  , module Language.Haskell.Tools.AST.Gen.Stmts
-  , module Language.Haskell.Tools.AST.Gen.Literals
-  , module Language.Haskell.Tools.AST.Gen.Patterns
-  , module Language.Haskell.Tools.AST.Gen.Names
-  , module Language.Haskell.Tools.AST.Gen.TH
-  , module Language.Haskell.Tools.AST.Match.Modules
-  , module Language.Haskell.Tools.AST.Match.Decls
-  , module Language.Haskell.Tools.AST.Match.Binds
-  , module Language.Haskell.Tools.AST.Match.Types
-  , module Language.Haskell.Tools.AST.Match.Kinds
-  , module Language.Haskell.Tools.AST.Match.Exprs
-  , module Language.Haskell.Tools.AST.Match.Stmts
-  , module Language.Haskell.Tools.AST.Match.Literals
-  , module Language.Haskell.Tools.AST.Match.Patterns
-  , module Language.Haskell.Tools.AST.Match.Names
-  , module Language.Haskell.Tools.AST.Match.TH
+  ( module Language.Haskell.Tools.AST.Gen
+  , module Language.Haskell.Tools.AST.Match
   , module Language.Haskell.Tools.AST.ElementTypes
   ) where
 
-import Language.Haskell.Tools.AST.Gen.Modules
-import Language.Haskell.Tools.AST.Gen.Decls
-import Language.Haskell.Tools.AST.Gen.Binds
-import Language.Haskell.Tools.AST.Gen.Types
-import Language.Haskell.Tools.AST.Gen.Kinds
-import Language.Haskell.Tools.AST.Gen.Exprs
-import Language.Haskell.Tools.AST.Gen.Stmts
-import Language.Haskell.Tools.AST.Gen.Literals
-import Language.Haskell.Tools.AST.Gen.Patterns
-import Language.Haskell.Tools.AST.Gen.Names
-import Language.Haskell.Tools.AST.Gen.TH
-
-import Language.Haskell.Tools.AST.Match.Modules
-import Language.Haskell.Tools.AST.Match.Decls
-import Language.Haskell.Tools.AST.Match.Binds
-import Language.Haskell.Tools.AST.Match.Types
-import Language.Haskell.Tools.AST.Match.Kinds
-import Language.Haskell.Tools.AST.Match.Exprs
-import Language.Haskell.Tools.AST.Match.Stmts
-import Language.Haskell.Tools.AST.Match.Literals
-import Language.Haskell.Tools.AST.Match.Patterns
-import Language.Haskell.Tools.AST.Match.Names
-import Language.Haskell.Tools.AST.Match.TH
-
+import Language.Haskell.Tools.AST.Gen
+import Language.Haskell.Tools.AST.Match
 import Language.Haskell.Tools.AST.ElementTypes
