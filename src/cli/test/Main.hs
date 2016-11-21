b57b30b4e47caa0a89e836edea3f6b6fd2d3da54
nboldi <nboldi@elte.hu>
2016-11-21 15:53:11 +0100

    Created a new interactive test for CLI

--- src/cli/test/Main.hs
+++ src/cli/test/Main.hs
@@ -2,7 +2,10 @@ module Main where
 
 import Test.HUnit
 import System.Exit
+import System.Directory
 import System.FilePath
+import Control.Monad (forM_)
+import Control.Exception
 import Data.Knob
 import Data.ByteString.Char8 (pack, unpack)
 import System.IO
@@ -23,6 +26,7 @@ allTests = map makeCliTest cliTests
 
 makeCliTest :: (String, [String], String, String) -> Test
 makeCliTest (dir, args, input, output) = TestLabel dir $ TestCase $ do 
+    copyDir dir (dir ++ "_orig")
     inKnob <- newKnob (pack input)
     inHandle <- newFileHandle inKnob "<input>" ReadMode
     outKnob <- newKnob (pack [])
@@ -30,6 +34,8 @@ makeCliTest (dir, args, input, output) = TestLabel dir $ TestCase $ do
     res <- refactorSession inHandle outHandle (args ++ [dir])
     actualOut <- Data.Knob.getContents outKnob
     assertEqual "" (filter (/= '\r') output) (filter (/= '\r') $ unpack actualOut)
+  `finally` do removeDirectoryRecursive dir
+               renameDirectory (dir ++ "_orig") dir
 
 cliTests :: [(String, [String], String, String)]
 cliTests 
@@ -45,8 +51,11 @@ cliTests
     , ( ".." </> ".." </> "examples" </> "Project" </> "has-cabal"
       , ["-dry-run", "-one-shot", "-module-name=A", "-refactoring=\"GenerateSignature 3:1-3:1\""] 
       , "", prefixText ["A"] ++ "### Module changed: A\n### new content:\nmodule A where\n\nx :: ()\nx = ()\n")
-    -- , ( ".." </> ".." </> "examples" </> "Refactor" </> "RenameDefinition" </> "MultiModule", [] 
-    --   , "", "")
+    , ( ".." </> ".." </> "examples" </> "Project" </> "reloading", [] 
+      , "SelectModule C\nRenameDefinition 3:1-3:2 cc\nSelectModule B\nRenameDefinition 5:1-5:2 bb\nExit"
+      , prefixText ["C","B","A"] ++ "no-module-selected> C> " 
+          ++ reloads ["C", "B", "A"] ++ "C> B> "
+          ++ reloads ["B", "A"] ++ "B> ")
     ]
 
 prefixText :: [String] -> String
@@ -54,3 +63,19 @@ prefixText mods
   = "Compiling modules. This may take some time. Please wait.\n" 
       ++ concatMap (\m -> "Loaded module: " ++ m ++ "\n") mods 
       ++ "All modules loaded. Use 'SelectModule module-name' to select a module\n"
+
+reloads :: [String] -> String
+reloads mods = concatMap (\m -> "Re-loaded module: " ++ m ++ "\n") mods 
+
+copyDir ::  FilePath -> FilePath -> IO ()
+copyDir src dst = do
+  createDirectory dst
+  content <- getDirectoryContents src
+  let xs = filter (`notElem` [".", ".."]) content
+  forM_ xs $ \name -> do
+    let srcPath = src </> name
+    let dstPath = dst </> name
+    isDirectory <- doesDirectoryExist srcPath
+    if isDirectory
+      then copyDir srcPath dstPath
+      else copyFile srcPath dstPath
