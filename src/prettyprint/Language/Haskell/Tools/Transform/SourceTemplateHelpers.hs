c2cb3fcbb34dfb8808f5d49227f1e9affcab751c
nboldi <nboldi@elte.hu>
2016-12-15 14:37:08 +0100

    fixed problems from merge messing up the code

--- src/prettyprint/Language/Haskell/Tools/Transform/SourceTemplateHelpers.hs
+++ src/prettyprint/Language/Haskell/Tools/Transform/SourceTemplateHelpers.hs
@@ -19,18 +19,13 @@ type ASTMulti node dom = AnnListG node dom SrcTemplateStage
 
 instance IsString (SpanInfo SrcTemplateStage) where
   fromString s = SourceTemplateNode noSrcSpan [TextElem s] 0 Nothing
-<<<<<<< HEAD
 
 -- * Basic elements
-=======
->>>>>>> master
-    
 child :: SpanInfo SrcTemplateStage
 child = SourceTemplateNode noSrcSpan [ChildElem] 0 Nothing
 
 opt :: OptionalInfo SrcTemplateStage
 opt = SourceTemplateOpt noSrcSpan "" "" 0 Nothing
-<<<<<<< HEAD
 
 list :: ListInfo SrcTemplateStage
 list = SourceTemplateList noSrcSpan "" "" "" False [] 0 Nothing
@@ -85,44 +80,6 @@ separatedBy sep = srcTmpDefaultSeparator .= sep
 -- | The elements of the list should be indented on the same column
 indented :: ListInfo SrcTemplateStage -> ListInfo SrcTemplateStage
 indented = (srcTmpIndented .= True) . (srcTmpDefaultSeparator .= "\n")
-=======
-
-optBefore :: String -> OptionalInfo SrcTemplateStage
-optBefore s = SourceTemplateOpt noSrcSpan s "" 0 Nothing
-
-optAfter :: String -> OptionalInfo SrcTemplateStage
-optAfter s = SourceTemplateOpt noSrcSpan "" s 0 Nothing
-
-optBeforeAfter :: String -> String -> OptionalInfo SrcTemplateStage
-optBeforeAfter bef aft = SourceTemplateOpt noSrcSpan bef aft 0 Nothing
-
-indentRelative :: Int -> OptionalInfo SrcTemplateStage -> OptionalInfo SrcTemplateStage
-indentRelative i = srcTmpOptRelPos .= Just i
-
-list :: ListInfo SrcTemplateStage
-list = SourceTemplateList noSrcSpan "" "" "" False [] 0 Nothing
-
-indentedList :: ListInfo SrcTemplateStage
-indentedList = SourceTemplateList noSrcSpan "" "" "\n" True [] 0 Nothing
-
-indentedListBefore :: String -> ListInfo SrcTemplateStage
-indentedListBefore bef = SourceTemplateList noSrcSpan bef "" "\n" True [] 0 Nothing
-
-indentedListAfter :: String -> ListInfo SrcTemplateStage
-indentedListAfter aft = SourceTemplateList noSrcSpan "" aft "\n" True [] 0 Nothing
-
-listSep :: String -> ListInfo SrcTemplateStage
-listSep s = SourceTemplateList noSrcSpan "" "" s False [] 0 Nothing
-
-listSepBefore :: String -> String -> ListInfo SrcTemplateStage
-listSepBefore s bef = SourceTemplateList noSrcSpan bef "" s False [] 0 Nothing
-
-listSepAfter :: String -> String -> ListInfo SrcTemplateStage
-listSepAfter s aft = SourceTemplateList noSrcSpan "" aft s False [] 0 Nothing
-
-listSepBeforeAfter :: String -> String -> String -> ListInfo SrcTemplateStage
-listSepBeforeAfter s bef aft = SourceTemplateList noSrcSpan bef aft s False [] 0 Nothing
->>>>>>> master
 
 -- | Concatenates two source templates to produce a new template with all child elements.
 (<>) :: SpanInfo SrcTemplateStage -> SpanInfo SrcTemplateStage -> SpanInfo SrcTemplateStage
