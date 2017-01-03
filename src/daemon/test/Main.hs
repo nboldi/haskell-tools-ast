4da13d731e1c5d76bc9ac9e2fef2f7121290079b
nboldi <nboldi@elte.hu>
2017-01-03 22:40:12 +0100

    give a little more time

--- src/daemon/test/Main.hs
+++ src/daemon/test/Main.hs
@@ -97,7 +97,7 @@ loadingTests =
 sourceRoot = ".." </> ".." </> "src"
 
 selfLoadingTest :: TestTree
-selfLoadingTest = localOption (mkTimeout ({- 3 min -} 1000 * 1000 * 60 * 3)) $ testCase "self-load" $ do  
+selfLoadingTest = localOption (mkTimeout ({- 5 min -} 1000 * 1000 * 60 * 5)) $ testCase "self-load" $ do  
     actual <- communicateWithDaemon 
                 [ Right $ AddPackages (map (sourceRoot </>) ["ast", "backend-ghc", "prettyprint", "rewrite", "refactor", "cli", "daemon"] ) ]
     assertBool ("The expected result is a nonempty response message list that does not contain errors. Actual result: " ++ show actual) 
