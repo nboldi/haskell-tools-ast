bb6c8990f4812e6d030600e7ab4ebd25658c64c0
nboldi <nboldi@elte.hu>
2017-02-11 15:21:37 +0100

    Simplified transformation of quasi-quotes.

--- src/backend-ghc/Language/Haskell/Tools/AST/FromGHC/TH.hs
+++ src/backend-ghc/Language/Haskell/Tools/AST/FromGHC/TH.hs
@@ -24,11 +24,12 @@ import qualified Language.Haskell.Tools.AST as AST
 
 trfQuasiQuotation' :: TransformName n r => HsSplice n -> Trf (AST.UQuasiQuote (Dom r) RangeStage)
  -- the lexer does not provide us with tokens '[', '|' and '|]'
-trfQuasiQuotation' (HsQuasiQuote id _ l str) 
-  = AST.UQuasiQuote <$> annLocNoSema (pure quoterLoc) (trfName' id)
-                   <*> annLocNoSema (pure strLoc) (pure $ AST.QQString (unpackFS str))
-  where quoterLoc = mkSrcSpan (updateCol (subtract (1 + length (occNameString $ rdrNameOcc $ rdrName id))) (srcSpanStart l)) 
-                              (updateCol (subtract 1) (srcSpanStart l))
+trfQuasiQuotation' (HsQuasiQuote id _ l str)
+  = AST.UQuasiQuote <$> annLocNoSema quoterLoc (trfName' id)
+                    <*> annLocNoSema (pure strLoc) (pure $ AST.QQString (unpackFS str))
+  where -- assume that there are no white spaces ain the head and the end of the quasi quote
+        quoterLoc = do rng <- asks contRange
+                       return $ mkSrcSpan (updateCol (+1) (srcSpanStart rng)) (updateCol (subtract 1) (srcSpanStart l))
         strLoc = mkSrcSpan (srcSpanStart l) (updateCol (subtract 2) (srcSpanEnd l))
 trfQuasiQuotation' _ = error "trfQuasiQuotation': splice received"
 
@@ -41,18 +42,18 @@ trfSplice' (HsUntypedSplice _ expr) = AST.UParenSplice <$> trfCorrectDollar expr
 trfSplice' (HsQuasiQuote {}) = error "trfSplice': quasi quotation received"
 
 trfCorrectDollar :: TransformName n r => Located (HsExpr n) -> Trf (Ann AST.UExpr (Dom r) RangeStage)
-trfCorrectDollar expr = 
+trfCorrectDollar expr =
   do isSplice <- allTokenLoc AnnThIdSplice
      case isSplice of [] -> trfExpr expr
-                      _  -> let newSp = updateStart (updateCol (+1)) (getLoc expr) 
+                      _  -> let newSp = updateStart (updateCol (+1)) (getLoc expr)
                              in case expr of L _ (HsVar (L _ varName)) -> trfExpr $ L newSp (HsVar (L newSp varName))
                                              L _ exp                   -> trfExpr $ L newSp exp
 
 trfBracket' :: TransformName n r => HsBracket n -> Trf (AST.UBracket (Dom r) RangeStage)
 trfBracket' (ExpBr expr) = AST.UExprBracket <$> trfExpr expr
 trfBracket' (TExpBr expr) = AST.UExprBracket <$> trfExpr expr
-trfBracket' (VarBr isSingle expr) 
-  = AST.UExprBracket <$> annLoc createScopeInfo (updateStart (updateCol (if isSingle then (+1) else (+2))) <$> asks contRange) 
+trfBracket' (VarBr isSingle expr)
+  = AST.UExprBracket <$> annLoc createScopeInfo (updateStart (updateCol (if isSingle then (+1) else (+2))) <$> asks contRange)
       (AST.UVar <$> (annContNoSema (trfName' expr)))
 trfBracket' (PatBr pat) = AST.UPatternBracket <$> trfPattern pat
 trfBracket' (DecBrL decls) = AST.UDeclsBracket <$> trfDecls decls
