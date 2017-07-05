592e202d1798bf04db46221413eae8864e98c52c
nboldi <nboldi@elte.hu>
2017-07-05 15:26:08 +0200

    Use the original problematic definition.

--- src/refactor/examples/TH/Splice/UseImported.hs
+++ src/refactor/examples/TH/Splice/UseImported.hs
@@ -1,4 +1,9 @@
 {-# LANGUAGE TemplateHaskell #-}
 module TH.Splice.UseImported where
 
-$( [d| f :: Show a => a -> String; f = show |] )
+data T a = T a
+
+$([d|
+  instance Show a => Show (T a) where show = undefined
+  instance (Eq a, Show a) => Eq (T a) where (==) = undefined
+  |])
