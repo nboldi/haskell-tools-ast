5fda9195705f06e7876765f196d9efa58ea64f08
nboldi <nboldi@elte.hu>
2017-08-10 10:11:02 +0200

    Don't reload after standalone perform refactoring.

--- demo/test/Main.hs
+++ demo/test/Main.hs
@@ -23,8 +23,8 @@ main = do -- create one daemon process for the whole testing session
 
 allTests :: TestTree
 allTests
-  = localOption (mkTimeout ({- 10s -} 1000 * 1000 * 10)) 
-      $ testGroup "daemon-tests" 
+  = localOption (mkTimeout ({- 10s -} 1000 * 1000 * 10))
+      $ testGroup "daemon-tests"
           [ testGroup "simple-tests" $ map makeDemoTest simpleTests
           , testGroup "loading-tests" $ map makeDemoTest loadingTests
           , testGroup "refactor-tests" $ map makeDemoTest refactorTests
@@ -32,7 +32,7 @@ allTests
           ]
 
 simpleTests :: [(String, [ClientMessage], [ResponseMsg])]
-simpleTests = 
+simpleTests =
   [ ( "empty-test", [], [] )
   , ( "keep-alive", [KeepAlive], [] )
   ]
@@ -61,49 +61,49 @@ refactorTests :: [(String, [ClientMessage], [ResponseMsg])]
 refactorTests =
   [ ( "simple-refactor"
     , [ InitialProject [("A", "module A where\n\na = ()")]
-      , PerformRefactoring "RenameDefinition" "A" "3:1-3:2" ["x"] ]
+      , PerformRefactoring "RenameDefinition" "A" "3:1-3:2" ["x"] False ]
     , [ RefactorChanges [("A", Just "module A where\n\nx = ()")] ] )
   , ( "multi-module-refactor"
     , [ InitialProject [("A", "module A where\n\na = ()"), ("B", "module B where\n\nimport A\n\nb = a")]
-      , PerformRefactoring "RenameDefinition" "A" "3:1-3:2" ["x"] ]
+      , PerformRefactoring "RenameDefinition" "A" "3:1-3:2" ["x"] False ]
     , [ RefactorChanges [("A", Just "module A where\n\nx = ()"), ("B", Just "module B where\n\nimport A\n\nb = x")] ] )
   , ( "multi-module-refactor-user-added-later"
     , [ InitialProject [("A", "module A where\n\na = ()")]
       , ModuleChanged "B" "module B where\n\nimport A\n\nb = a"
-      , PerformRefactoring "RenameDefinition" "A" "3:1-3:2" ["x"] ]
+      , PerformRefactoring "RenameDefinition" "A" "3:1-3:2" ["x"] False ]
     , [ RefactorChanges [("A", Just "module A where\n\nx = ()"), ("B", Just "module B where\n\nimport A\n\nb = x")] ] )
   , ( "multi-module-refactor-both-added-later"
     , [ InitialProject [("B", "module B where")]
       , ModuleChanged "A" "module A where\n\na = ()"
       , ModuleChanged "B" "module B where\n\nimport A\n\nb = a"
-      , PerformRefactoring "RenameDefinition" "A" "3:1-3:2" ["x"] ]
+      , PerformRefactoring "RenameDefinition" "A" "3:1-3:2" ["x"] False ]
     , [ RefactorChanges [("A", Just "module A where\n\nx = ()"), ("B", Just "module B where\n\nimport A\n\nb = x")] ] )
   , ( "rename-module"
     , [ InitialProject [("A", "module A where\n\na = ()")]
-      , PerformRefactoring "RenameDefinition" "A" "1:8-1:9" ["AA"] ]
+      , PerformRefactoring "RenameDefinition" "A" "1:8-1:9" ["AA"] False ]
     , [ RefactorChanges [("AA", Just "module AA where\n\na = ()"), ("A", Nothing)] ] )
   , ( "change-module"
     , [ InitialProject [("A", "module A where\n\na = ()")]
       , ModuleChanged "A" "module A where\n\n\na = ()"
-      , PerformRefactoring "RenameDefinition" "A" "4:1-4:2" ["x"] ]
+      , PerformRefactoring "RenameDefinition" "A" "4:1-4:2" ["x"] False ]
     , [ RefactorChanges [("A", Just "module A where\n\n\nx = ()")] ] )
   , ( "removed-module"
     , [ InitialProject [("A", "module A where\n\na = ()"), ("B", "module B where\n\nimport A\n\nb = a")]
       , ModuleDeleted "B"
-      , PerformRefactoring "RenameDefinition" "A" "3:1-3:2" ["x"] ]
+      , PerformRefactoring "RenameDefinition" "A" "3:1-3:2" ["x"] False ]
     , [ RefactorChanges [("A", Just "module A where\n\nx = ()")] ] )
   ]
 
 makeDemoTest :: (String, [ClientMessage], [ResponseMsg]) -> TestTree
-makeDemoTest (label, input, expected) = testCase label $ do  
+makeDemoTest (label, input, expected) = testCase label $ do
     actual <- communicateWithDemo input
     assertEqual "" expected actual
 
 astViewTest :: TestTree
-astViewTest = testCase "ast-view-test" $ do  
+astViewTest = testCase "ast-view-test" $ do
     actual <- communicateWithDemo [ InitialProject [("A", "module A where\n\na = ()")]
                                   , PerformRefactoring "UpdateAST" "A" "1:1" [] ]
-    assertBool "The response must be a simple ASTViewContent message" 
+    assertBool "The response must be a simple ASTViewContent message"
       $ case actual of [ ASTViewContent _ ] -> True
                        _                    -> False
 
