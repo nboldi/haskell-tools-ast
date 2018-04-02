9fd037131e46761af23fbfcf3f0a423f42c161e4
ktomi996 <33902410+ktomi996@users.noreply.github.com>
2018-04-02 13:26:26 +0200

    Add files via upload

---
+++ Builtin.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE FlexibleContexts, MonoLocalBinds #-}
+
+module Language.Haskell.Tools.Refactor.Builtin ( builtinRefactorings, builtinQueries ) where
+
+import Language.Haskell.Tools.Refactor (RefactoringChoice, QueryChoice)
+import Language.Haskell.Tools.Refactor.Builtin.ExtractBinding (extractBindingRefactoring)
+import Language.Haskell.Tools.Refactor.Builtin.FloatOut (floatOutRefactoring)
+import Language.Haskell.Tools.Refactor.Builtin.GenerateExports (generateExportsRefactoring)
+import Language.Haskell.Tools.Refactor.Builtin.GenerateTypeSignature (generateTypeSignatureRefactoring)
+import Language.Haskell.Tools.Refactor.Builtin.InlineBinding (inlineBindingRefactoring)
+import Language.Haskell.Tools.Refactor.Builtin.OrganizeExtensions (organizeExtensionsRefactoring, projectOrganizeExtensionsRefactoring)
+import Language.Haskell.Tools.Refactor.Builtin.OrganizeImports (organizeImportsRefactoring, projectOrganizeImportsRefactoring)
+import Language.Haskell.Tools.Refactor.Builtin.RenameDefinition (renameDefinitionRefactoring)
+import Language.Haskell.Tools.Refactor.Builtin.GetMatches (getMatchesQuery)
+import Language.Haskell.Tools.Refactor.Builtin.AutoCorrect
+import Language.Haskell.Tools.Refactor.Builtin.FindUsages
+import Language.Haskell.Tools.Refactor.Builtin.GetTypeInfo
+import Language.Haskell.Tools.Refactor.Builtin.JumpToDefinition
+
+builtinRefactorings :: [RefactoringChoice]
+builtinRefactorings
+  = [ organizeImportsRefactoring
+    , projectOrganizeImportsRefactoring
+    , inlineBindingRefactoring
+    , generateTypeSignatureRefactoring
+    , renameDefinitionRefactoring
+    , generateExportsRefactoring
+    , floatOutRefactoring
+    , extractBindingRefactoring
+    , organizeExtensionsRefactoring
+    , projectOrganizeExtensionsRefactoring
+    , autoCorrectRefactoring
+    ]
+
+builtinQueries :: [QueryChoice]
+builtinQueries = [ getMatchesQuery , getUsagesQuery , getTypeInfo, getDefinitionQuery]
