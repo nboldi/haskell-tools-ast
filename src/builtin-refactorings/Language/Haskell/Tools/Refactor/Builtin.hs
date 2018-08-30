e3a8f6d77bbc1c961f696dfaa36bd70b06cc7131
ktomi996 <33902410+ktomi996@users.noreply.github.com>
2018-08-30 22:57:53 +0200

    Update Builtin.hs

--- src/builtin-refactorings/Language/Haskell/Tools/Refactor/Builtin.hs
+++ src/builtin-refactorings/Language/Haskell/Tools/Refactor/Builtin.hs
@@ -1,20 +1,26 @@
-{-# LANGUAGE FlexibleContexts, TypeFamilies #-}
-module Language.Haskell.Tools.Refactor.Builtin ( builtinRefactorings ) where
+{-# LANGUAGE FlexibleContexts, MonoLocalBinds #-}
 
-import Language.Haskell.Tools.Refactor (RefactoringChoice)
-import Language.Haskell.Tools.Refactor.Builtin.ExtractBinding (ExtractBindingDomain, extractBindingRefactoring)
+module Language.Haskell.Tools.Refactor.Builtin ( builtinRefactorings, builtinQueries ) where
+
+import Language.Haskell.Tools.Refactor (RefactoringChoice, QueryChoice)
+import Language.Haskell.Tools.Refactor.Builtin.ExtractBinding (extractBindingRefactoring)
 import Language.Haskell.Tools.Refactor.Builtin.FloatOut (floatOutRefactoring)
-import Language.Haskell.Tools.Refactor.Builtin.GenerateExports (DomGenerateExports, generateExportsRefactoring)
-import Language.Haskell.Tools.Refactor.Builtin.GenerateTypeSignature (GenerateSignatureDomain, generateTypeSignatureRefactoring)
+import Language.Haskell.Tools.Refactor.Builtin.GenerateExports (generateExportsRefactoring)
+import Language.Haskell.Tools.Refactor.Builtin.GenerateTypeSignature (generateTypeSignatureRefactoring)
 import Language.Haskell.Tools.Refactor.Builtin.InlineBinding (inlineBindingRefactoring)
-import Language.Haskell.Tools.Refactor.Builtin.OrganizeExtensions (OrganizeExtensionsDomain, organizeExtensionsRefactoring, projectOrganizeExtensionsRefactoring)
-import Language.Haskell.Tools.Refactor.Builtin.OrganizeImports (OrganizeImportsDomain, organizeImportsRefactoring, projectOrganizeImportsRefactoring)
-import Language.Haskell.Tools.Refactor.Builtin.RenameDefinition (DomainRenameDefinition, renameDefinitionRefactoring)
+import Language.Haskell.Tools.Refactor.Builtin.OrganizeExtensions (organizeExtensionsRefactoring, projectOrganizeExtensionsRefactoring)
+import Language.Haskell.Tools.Refactor.Builtin.OrganizeImports (organizeImportsRefactoring, projectOrganizeImportsRefactoring)
+import Language.Haskell.Tools.Refactor.Builtin.RenameDefinition (renameDefinitionRefactoring)
+import Language.Haskell.Tools.Refactor.Builtin.GetMatches (getMatchesQuery)
+import Language.Haskell.Tools.Refactor.Builtin.AutoCorrect
+import Language.Haskell.Tools.Refactor.Builtin.FindUsages
+import Language.Haskell.Tools.Refactor.Builtin.GetTypeInfo
+import Language.Haskell.Tools.Refactor.Builtin.JumpToDefinition
+import Language.Haskell.Tools.Refactor.Builtin.DefinedInThis
+import Language.Haskell.Tools.Refactor.Builtin.DefinedModuleInfos
+import Language.Haskell.Tools.Refactor.Builtin.Scope
 
-builtinRefactorings :: ( DomGenerateExports dom, OrganizeImportsDomain dom
-                       , DomainRenameDefinition dom, ExtractBindingDomain dom
-                       , GenerateSignatureDomain dom, OrganizeExtensionsDomain dom
-                       ) => [RefactoringChoice dom]
+builtinRefactorings :: [RefactoringChoice]
 builtinRefactorings
   = [ organizeImportsRefactoring
     , projectOrganizeImportsRefactoring
@@ -26,4 +32,8 @@ builtinRefactorings
     , extractBindingRefactoring
     , organizeExtensionsRefactoring
     , projectOrganizeExtensionsRefactoring
+    , autoCorrectRefactoring
     ]
+
+builtinQueries :: [QueryChoice]
+builtinQueries = [ getMatchesQuery , getUsagesQuery , getTypeInfo, getDefinitionQuery, defined, definedModule, getScope]
