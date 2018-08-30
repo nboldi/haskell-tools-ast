46033e977db4042eb45646367f80535aae175d74
ktomi996 <33902410+ktomi996@users.noreply.github.com>
2018-08-30 22:30:12 +0200

    Update DefinedModuleInfos.hs

--- src/builtin-refactorings/Language/Haskell/Tools/Refactor/Builtin/DefinedModuleInfos.hs
+++ src/builtin-refactorings/Language/Haskell/Tools/Refactor/Builtin/DefinedModuleInfos.hs
@@ -16,13 +16,23 @@ import Control.Monad.State
 import SrcLoc as GHC
 import Debug.Trace (trace)
 
+--Result of this function is that every time when user runs DefinedInfo command
+--the definedInfo function will be invoked.
 
 definedModule :: QueryChoice
 definedModule = LocationQuery "DefinedInfo" definedInfo
 
+--Returns the result of defMods wrapped by JSON and QueryMonad.
+
 definedInfo :: RealSrcSpan -> ModuleDom -> [ModuleDom] -> QueryMonad Value
 definedInfo _ mod _
   = return $ toJSON $ defMods mod
+
+--Returns the appearances of unique GHC.Names
+--Paramteres:
+--x:ModuleDom: The modul from which we would like to get appearances of Names.
+--Result:[(qualifiedName, short name, position)]
+--where position [start line,start column, end line, end column]
 
 defMods :: ModuleDom -> [(String, String, [Int])]
 defMods x = getUnique (nameAndPlace qualifiedNames) isEqualQualif isLesser
@@ -30,17 +40,24 @@ defMods x = getUnique (nameAndPlace qualifiedNames) isEqualQualif isLesser
     qualifiedNames :: [QualifiedName]
     qualifiedNames = ((snd x) ^? biplateRef)
 
+--Returns the appearances of GHC.Names
+--Paramteres:
+--[QualifiedName]: The of QualifiedName from which we get the information
+--Result:[(qualifiedName, short name, position)]
+--where position [start line,start column, end line, end column]
 
 nameAndPlace :: [QualifiedName] -> [(String, String, [Int])]
 nameAndPlace [] = []
 nameAndPlace (x:xs) = (outPutInfo x) ++ nameAndPlace xs
 
-{-
-getUnique :: [(String, String, [Int])] -> [(String, String, [Int])]
-getUnique x = getHead $ groupBy isEqualQualif $ sortBy isLesser x
--}
+--Returns the position to SrcSpan if it is RealSrcSpan
+--position: [start line,start column, end line, end column]
+
 getPlaceInCode :: SrcSpan -> [Int]
 getPlaceInCode (RealSrcSpan sp) = [srcSpanStartLine sp, srcSpanStartCol sp, srcSpanEndLine sp, srcSpanEndCol sp]
+
+--If GHC.Name belongs to the QualifiedName this function returns its string representation
+--Otherwise returns an empty list
 
 outPutInfo :: QualifiedName -> [(String, String, [Int])]
 outPutInfo x
@@ -49,4 +66,3 @@ outPutInfo x
  where
   sp = getRange x
   name = getNameToList x
-
